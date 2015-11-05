78e21e8ca82e79df889e6f789903c642f22bd154
Matt Stone <mstone@chgr.mgh.harvard.edu>
2015-11-05 08:47:29 -0500

    Vim templates

---
+++ vim/templates/=template=.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright © %YEAR% %NAME% <%MAIL%>
+# Distributed under terms of the %LICENSE% license.
+
+"""
+%HERE%
+"""
+
+import argparse
+
+
+def %FILE%():
+    pass
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="")
+
+    args = parser.parse_args()
+
+    %FILE%()
+
+
+if __name__ == '__main__':
+    main()
---
+++ vim/templates/=template=.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# %FFILE%
+#
+# %HERE%
+#
+# Copyright (C) %YEAR% %NAME% <%MAIL%>
+# Distributed under terms of the %LICENSE% license.
+
+
