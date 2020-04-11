2caae6334b53c3136a8f3daab3053eece526a95d
Matt Stone <matthew.stone12@gmail.com>
2020-04-11 13:36:53 -0500

    add usage and getops to bash template

--- vim/templates/=template=.sh
+++ vim/templates/=template=.sh
@@ -7,4 +7,31 @@
 # Copyright (C) %YEAR% %NAME% <%MAIL%>
 # Distributed under terms of the %LICENSE% license.
 
+usage() {
+  cat <<EOF
+usage: %FFILE% [-h]
+
+<DESC>
+
+Optional arguments:
+  -h        Show this help message and exit.
+EOF
+}
+
+while getopts ":h" opt; do
+  case ${opt} in
+    h)
+      usage
+      exit 0
+      ;;
+    *)
+      echo "Invalid option" 1>&2
+      usage
+      exit 1
+      ;;
+  esac
+done
+
+shift $((OPTIND - 1))
+
 
